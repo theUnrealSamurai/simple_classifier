bf91f065152fbad617dbb976cc3f7d856a271a69
江口 天 <t_eguchi@YRL73-81368.local>
2023-08-09 14:25:32 +0900

    addded core ml convertion script and some swift resources

--- classifier/core_ml_converter.py
+++ classifier/core_ml_converter.py
@@ -1,9 +1,11 @@
 import coremltools as ct
 from tensorflow import keras 
+
+# Keras model convertion logic...
 model = keras.models.load_model('./sl_model.h5')
 print(model)
 
 mlmodel = ct.convert(model, inputs=[ct.ImageType(scale=1/127, shape=[1,224,224,3])])
 print(mlmodel)
 
-mlmodel.save("Test.mlmodel")
+mlmodel.save("Test2.mlmodel")
