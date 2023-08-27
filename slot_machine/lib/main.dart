a7d701f294d7cf52af9de89fe0fda0f5ac43c41a
Roshan Saravanan <roshan.saravanan001@gmail.com>
2023-08-27 20:18:09 +0530

    Finished the app and tested it in the phone

--- slot_machine/lib/main.dart
+++ slot_machine/lib/main.dart
@@ -1,25 +1,32 @@
 import 'package:flutter/material.dart';
-import 'get_image_from_camera.dart';
-import 'package:camera/camera.dart';
+import 'home_screen.dart';
 
-Future<void> main() async {
-  // Ensure that plugin services are initialized so that `availableCameras()`
-  // can be called before `runApp()`
-  WidgetsFlutterBinding.ensureInitialized();
+void main() {
+  runApp(const MyApp());
+}
 
-  // Obtain a list of the available cameras on the device.
-  final cameras = await availableCameras();
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  // Get a specific camera from the list of available cameras.
-  final firstCamera = cameras.first;
-
-  runApp(
-    MaterialApp(
-      theme: ThemeData.dark(),
-      home: TakePictureScreen(
-        // Pass the appropriate camera to the TakePictureScreen widget.
-        camera: firstCamera,
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'OBJECT DETECTOR',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
-    ),
-  );
+      home: const HomeScreen(),
+    );
+  }
 }
